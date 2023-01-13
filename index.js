893eba0ff24fb49d8ee4820e106b8840cd660715
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-13 11:21:30 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+// use same alert message on multiple elements 
+//create a separate function that could by called by all event listeners 
+//pass the function name as the second argument to addEventListener
+
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
